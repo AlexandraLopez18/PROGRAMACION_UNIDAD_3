1df36d24f79bdf369c83753fe0eed24441c36a72
AlexandraLopez18 <azulalexandrlopez@gmail.com>
2025-04-04 21:45:43 -0500

    Add files via upload

---
+++ gif programacion/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace gif_programacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Yu Mincho Demibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(330, 171);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(263, 51);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Programacion Basica";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(398, 243);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 123);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 192, 255);
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(988, 616);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            ForeColor = Color.White;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+    }
+}
